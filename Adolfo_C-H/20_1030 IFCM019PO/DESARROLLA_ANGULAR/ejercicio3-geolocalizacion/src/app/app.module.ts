24f5ddef81d9280d4d3dc64d07276258311d68c3
Adolfodiv.#/Adolfo <126599475+Adolfo-C@users.noreply.github.com>
2023-04-11 18:42:57 +0200

    Add files via upload

---
+++ Adolfo_C-H/20_1030 IFCM019PO/DESARROLLA_ANGULAR/ejercicio3-geolocalizacion/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import {AgmCoreModule} from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatBadgeModule,
+    AgmCoreModule.forRoot({
+      apiKey:''
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
