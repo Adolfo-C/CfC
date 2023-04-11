3a27733462a5e1e2f270ef62af9640c92720c863
Adolfodiv.#/Adolfo <126599475+Adolfo-C@users.noreply.github.com>
2023-04-11 19:34:33 +0200

    Add files via upload

---
+++ Adolfo_C-H/20_1030 IFCM019PO/DESARROLLA_ANGULAR/2-ANGULAR_DINAMICA/ejercicio4-I18n-R/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
