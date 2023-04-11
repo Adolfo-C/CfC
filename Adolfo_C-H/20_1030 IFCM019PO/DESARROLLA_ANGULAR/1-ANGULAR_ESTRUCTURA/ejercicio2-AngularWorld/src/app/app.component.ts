73aa32c4cabeef8c850b4b63b404e8c0e202a764
Adolfodiv.#/Adolfo <126599475+Adolfo-C@users.noreply.github.com>
2023-04-11 18:08:40 +0200

    Add files via upload

---
+++ Adolfo_C-H/20_1030 IFCM019PO/DESARROLLA_ANGULAR/1-ANGULAR_ESTRUCTURA/ejercicio2-AngularWorld/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DESARROLLADO POR CfC Ach';
+  titulo:string = 'CURSO DE PROGRAMACIÓN EN TECNOLOGÍA MÓVIL.'
+}
