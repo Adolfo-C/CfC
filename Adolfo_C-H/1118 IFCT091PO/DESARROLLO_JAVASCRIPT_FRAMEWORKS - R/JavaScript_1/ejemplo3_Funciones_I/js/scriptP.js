8c111e9e877ae74894469a5acca22e03f94ae5e6
Adolfodiv.#/Adolfo <126599475+Adolfo-C@users.noreply.github.com>
2023-04-15 19:33:52 +0200

    Delete Adolfo_C-H/1118 IFCT091PO/DESARROLLO_JAVASCRIPT_FRAMEWORKS - R directory

--- Adolfo_C-H/1118 IFCT091PO/DESARROLLO_JAVASCRIPT_FRAMEWORKS - R/JavaScript_1/ejemplo3_Funciones_I/js/scriptP.js
+++
@@ -1,7 +0,0 @@
-function hora() {
-    var hora = new Date();
-    document.getElementById('resultado').innerHTML= hora.toLocaleTimeString();
-}
-function limpiar() {
-    document.getElementById('resultado').innerHTML= '';
-}
